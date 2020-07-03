e1f216333d6cc5284b6de0aa69942f22a7a79503
Adrian M.A <adrian@neurocadi.es>
2020-07-03 23:55:48 +0200

    Reorganizing folders and fixing problems

--- frontendMovies/src/app/register/register.component.ts
+++ frontendMovies/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from "../user.service";
+import { UserService } from "../services/user.service";
 
 @Component({
   selector: 'app-register',
@@ -18,7 +18,9 @@ export class RegisterComponent implements OnInit {
     Role:"user"
   };
   Password2:string;
-  message="";
+  messageWrong: string = "No puede registrarse con ese Email";
+  messageRight: string = "Se ha registrado con éxito";
+  notRegister="";
 
   constructor(private UserService:UserService) { }
 
@@ -26,35 +28,16 @@ export class RegisterComponent implements OnInit {
   }
 
   sendRegister(){
-    let error = this.validate();
-    if (error) {
-      alert(error);
-      return;
-    }
     this.UserService.userRegister(this.userForm)
     .subscribe({
-      next: data => alert ("Usuario registrado correctamente"),
-      error: error => console.log(error)
-    });    
-  }
-
-  validate(): string {
-    let error = "";
-    let regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-
-
-    if (this.userForm.Password == this.Password2) {
-      if (this.userForm.Password.length < 8) {
-        error += " The password must be longer than 8 characters. ";
+      next: data => {
+        alert ("Usuario registrado correctamente");
+        console.log(data);
+      },  
+      error: error => {
+        alert("No puede registrarse con ese Email.");
+        console.log(error);
       }
-      //password contains symbol
-      //password contains upper and lowercase
-    } else {
-      error += "The passwords don't match.";
-    }
-    if (!this.userForm.Email.match(regexp)) {
-      error += "The email have an incorrect format.";
-    }
-    return error;
+    });
   }
 }
